77abbb18d6698ec170374d333c57c0b5a8a81b8a
dsdashun <dsdashun@qq.com>
2024-04-19 13:55:36 +0800

    feat(rcli): add csv cli support

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// rcli csv -i input.csv -o output.json --header -d ','
+use clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts: Opts = Opts::parse();
+    println!("{:?}", opts);
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
+}
