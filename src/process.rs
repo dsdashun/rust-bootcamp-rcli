a69338385838e26d0f6860d90a2139b9a271d04f
dsdashun <dsdashun@qq.com>
2024-04-23 13:49:13 +0800

    feat: rcli csv 支持 output format

--- src/process.rs
+++ src/process.rs
@@ -1,3 +1,4 @@
+use crate::opts::OutputFormat;
 use anyhow::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
@@ -16,7 +17,7 @@ struct Player {
     kit: u8,
 }
 
-pub fn process_csv(input: &str, output: &str) -> Result<()> {
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(100);
     let headers = reader.headers()?.clone();
@@ -30,7 +31,10 @@ pub fn process_csv(input: &str, output: &str) -> Result<()> {
         println!("{:?}", record);
         ret.push(json_record);
     }
-    let json = serde_json::to_string_pretty(&ret)?;
-    fs::write(output, json)?;
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+    };
+    fs::write(output, content)?;
     Ok(())
 }
