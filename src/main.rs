62cf93739938e1c0cadc1bfa3b2e25bd030727ee
dsdashun <dsdashun@qq.com>
2024-04-23 23:47:16 +0800

    feat(rcli): add random generator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // rcli csv -i input.csv -o output.json --header -d ','
 use clap::Parser;
-use rcli::{process_csv, Opts, SubCommand};
+use rcli::{process_csv, process_genpass, Opts, SubCommand};
 
 fn main() -> anyhow::Result<()> {
     let opts: Opts = Opts::parse();
@@ -13,6 +13,16 @@ fn main() -> anyhow::Result<()> {
                 format!("output.{}", opts.format)
             };
             process_csv(&opts.input, output, opts.format)?;
+        }
+        SubCommand::GenPass(opts) => {
+            println!("Generating password with options: {:?}", opts);
+            process_genpass(
+                opts.length,
+                opts.uppercase,
+                opts.lowercase,
+                opts.numbers,
+                opts.symbols,
+            )?;
         }
     }
     Ok(())
---
+++ src/process/gen_pass.rs
@@ -0,0 +1,44 @@
+use rand::seq::SliceRandom;
+
+const UPPER: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
+const LOWER: &[u8] = b"abcdefghijkmnpqrstuvwxyz";
+const NUMBERS: &[u8] = b"123456789";
+const SYMBOLS: &[u8] = b"!@#$%^&*_";
+
+pub fn process_genpass(
+    length: u8,
+    upper: bool,
+    lower: bool,
+    numbers: bool,
+    symbols: bool,
+) -> anyhow::Result<()> {
+    let mut password = Vec::new();
+    let mut rng = rand::thread_rng();
+    let mut chars = Vec::new();
+    if upper {
+        chars.extend_from_slice(UPPER);
+        password.push(*UPPER.choose(&mut rng).expect("UPPER wont be empty"));
+    }
+    if lower {
+        chars.extend_from_slice(LOWER);
+        password.push(*LOWER.choose(&mut rng).expect("LOWER wont be empty"));
+    }
+    if numbers {
+        chars.extend_from_slice(NUMBERS);
+        password.push(*NUMBERS.choose(&mut rng).expect("NUMBERS wont be empty"));
+    }
+    if symbols {
+        chars.extend_from_slice(SYMBOLS);
+        password.push(*SYMBOLS.choose(&mut rng).expect("SYMBOLS wont be empty"));
+    }
+    password.shuffle(&mut rng);
+    for _ in 0..length - password.len() as u8 {
+        //let idx = rng.gen_range(0..chars.len());
+        let c = chars
+            .choose(&mut rng)
+            .expect("chars wont be empty in this case");
+        password.push(*c);
+    }
+    println!("{}", String::from_utf8(password)?);
+    Ok(())
+}
