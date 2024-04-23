166f944cb88aa55e15cef3bfe2d78a0b6f9c2a7e
dsdashun <dsdashun@qq.com>
2024-04-23 13:30:05 +0800

    feat: support parsing generic CSV structure

--- src/process.rs
+++ src/process.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fs;
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -18,10 +19,16 @@ struct Player {
 pub fn process_csv(input: &str, output: &str) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(100);
-    for result in reader.deserialize() {
-        let player: Player = result?;
-        println!("{:?}", player);
-        ret.push(player);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        // headers.iter() => 使用 headers 的迭代器
+        // record.iter() => 使用 record 的迭代器
+        // zip => 将两个迭代器合并成一个元组的迭代器 [(header1, record1), (header2, record2), ...]
+        // collect::<Value>() => 将元组的迭代器转换成 Value 类型
+        let json_record = headers.iter().zip(record.iter()).collect::<Value>();
+        println!("{:?}", record);
+        ret.push(json_record);
     }
     let json = serde_json::to_string_pretty(&ret)?;
     fs::write(output, json)?;
